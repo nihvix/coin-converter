4bd04c5da7d776d5e28cc72c345e564a81f21707
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-04 13:51:35 +0200

    Base JS (don't work properly)

--- app.js
+++ app.js
@@ -7,3 +7,54 @@ EUR= 1,08 DOLLAR
 EUR= 1490,89 WON
 EUR= 174,20 YEN
 */
+/*----- Variables -----*/
+const inputEuro = document.getElementById("euro");
+const inputCurrency = document.getElementById("formCurrency");
+const outputCurrency = document.getElementById("output");
+const convertButton = document.getElementById("convert");
+const error = document.getElementById("error");
+
+
+/*----- Listener -----*/
+convertButton.addEventListener('click', convert);
+
+/*----- Functions -----*/
+function checkInfo() {
+  if (inputEuro.value.length == 0) {
+    inputEuro.focus();
+    error.innerText = "Amount field cannot be empty";
+    return false;
+  } else if (isNaN(inputEuro.value)) {
+    inputEuro.focus();
+    error.innerText = "Amount must be a number";
+    return false;
+  } else if (parseInt(inputEuro.value) < 0) {
+    inputEuro.focus();
+    error.innerText = "Amount must be greater than 0";
+    return false;
+  } else if (inputCurrency.value == "0") {
+    inputCurrency.focus();
+    error.innerText = "Select a currency";
+    return false;
+  }
+  return true;
+}
+
+function convert() {
+  if (checkInfo()) {
+    let money = parseInt(inputEuro.value);
+    switch (inputCurrency.value) {
+      case "dol":
+        outputCurrency.value = (money * 1.08) + " $";
+        break;
+      case "won":
+        outputCurrency.value = (money * 1490.89) + " ₩";
+        break;
+      case "yen":
+        outputCurrency.value = (money * 174.20) + " ¥";
+        break;
+      default: break;
+    }
+  }
+}
+
